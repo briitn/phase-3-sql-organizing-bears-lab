17540b32fbd7c52f9f51ce40fd9d1d76baf11da4
princegobah <babynamek@gmail.com>
2022-06-12 15:30:21 -0400

    commit tree b49545cecd8bd423740236b58ae23555e331700f

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'cool', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 8, 'M', 'brown', 'cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F', 'black', 'hot', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 6, 'M', 'brown', 'hot', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 2, 'F', 'brown', 'cool', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'black', 'cool', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 9, 'F', 'black', 'cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 0, 'M', 'black', 'cool', 1);
